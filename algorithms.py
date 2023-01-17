6963d93d707eb47fe01a85bc9b08f99fc0f4ced3
iskowan <iskowan@ssfs.org>
2023-01-17 11:40:38 -0500

    Commit 3

--- algorithms.py
+++ algorithms.py
@@ -1,9 +1,4 @@
-numbers = [1, 2, 3, 2, 5, 3, 3, 5, 6, 3, 4, 5, 7]
 
-duplicates = [number for number in numbers if numbers.count(number) > 1]
-unique_duplicates = list(set(duplicates))
-
-print(unique_duplicates)
 def add_numbers(numbers):
     """
     Ex. add_numbers([9, 5, 11, 6, 1, 15]) returns 47
